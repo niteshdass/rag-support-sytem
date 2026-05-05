60b9a7cae7da53340cf021386ec9aeb7f0e5d5e6
Nitesh Das <dasnitesh780@gmail.com>
2026-05-05 08:28:34 +0600

    feat(web): wire AppShell layout route + all protected page routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AppShell from './components/AppShell'
 import ProtectedRoute from './components/ProtectedRoute'
+import Activity from './pages/Activity'
 import Dashboard from './pages/Dashboard'
+import Documents from './pages/Documents'
 import Login from './pages/Login'
+import Settings from './pages/Settings'
+import Sources from './pages/Sources'
+import Upload from './pages/Upload'
 
 export default function App() {
   return (
@@ -9,7 +15,14 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Dashboard />} />
+          <Route element={<AppShell />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/sources" element={<Sources />} />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
